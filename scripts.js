5cc3a5c38444f08f60c1a8c91d1e6f3ea4496e40
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-05-05 21:15:05 -0400

    Added animation for skill section

---
+++ scripts.js
@@ -0,0 +1,11 @@
+!(function ($) {
+    $('.skills-content').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {
+        offset: '75%'
+    });
+
+
+})(jQuery);
